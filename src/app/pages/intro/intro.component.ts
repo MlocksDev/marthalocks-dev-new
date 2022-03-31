c25e1e3c3c18b70346e822ca7662ad63f4d53a24
martha <martha.locks@alibin.com.br>
2022-03-31 19:19:28 -0300

    Add GLightbox.

--- src/app/pages/intro/intro.component.ts
+++ src/app/pages/intro/intro.component.ts
@@ -1,5 +1,5 @@
 import { Component, OnInit } from '@angular/core';
-
+declare var GLightbox: any;
 @Component({
   selector: 'app-intro',
   templateUrl: './intro.component.html',
@@ -10,6 +10,19 @@ export class IntroComponent implements OnInit {
   constructor() { }
 
   ngOnInit(): void {
+
+    this.initGLightbox();
+  }
+
+  private initGLightbox(): void {
+    GLightbox({
+      'touchNavigation': true,
+      'href': 'https://www.youtube.com/watch?v=J9lS14nM1xg',
+      'type': 'video',
+      'source': 'youtube', //vimeo, youtube or local
+      'width': 900,
+      'autoPlayVideos': 'true',
+    });
   }
 
 }
