8040cac6e392efbb59e2e9aee63e0aa06f3dc6d6
martha <martha.locks@alibin.com.br>
2022-04-02 15:23:08 -0300

    Remove unused files.

--- src/app/components/campanies/campanies.component.ts
+++ src/app/components/campanies/campanies.component.ts
@@ -2,8 +2,7 @@ import { Component, OnInit } from '@angular/core';
 
 @Component({
   selector: 'app-campanies',
-  templateUrl: './campanies.component.html',
-  styleUrls: ['./campanies.component.scss']
+  templateUrl: './campanies.component.html'
 })
 export class CampaniesComponent implements OnInit {
 
